554e1f31529d351f7985729ceb1a120bd206a9c5
The Prof <52168158+computationalknowledge@users.noreply.github.com>
2021-11-02 08:36:41 -0400

    Update activity1.js

--- activity1.js
+++ activity1.js
@@ -6,3 +6,17 @@ function add(x,y){return x+y;}
 a = 7
 
 console.log('fini');
+
+
+//  *******************************
+
+var a = 7;
+console.log(add(2,2));
+//  hoisting
+//  temporal dead zone
+console.log(" a is ", a);
+function add(x,y){return a+y;}
+
+var a =  7 ;
+
+console.log('fini');
